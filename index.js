28ef04a622c4bc2af84b9a7b364603dcb36e7132
nattatorn.dev <nattatorn.dev@gmail.com>
2017-10-15 17:41:18 +0700

    :art: Replace Object.assign() to Object Spread Operator

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ module.exports = unwind;
 function unwind(array, prop) {
   return array
     .reduce((acc, curr) => [...acc, ...curr[prop]
-      .map(x => Object
-        .assign({}, curr, { [prop]: x }),
+      .map(x => ({curr, ...{ [prop]: x }})
       )], 
     []);
 }
