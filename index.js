8e2161d05cc99fdd93b3476581d2f01ca4ae6a15
Gustavo Gondim <gustavospgondim@gmail.com>
2017-10-16 17:09:11 -0200

    Added support to ES6 modules

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-module.exports = unwind;
-
-function unwind(array, path) {
-  if (!array.length) {
-    array = [ array ];
+(function index() {
+  function unwind(array, path) {
+    if (!array.length) {
+      array = [array];
+    }
+    if (path.indexOf('.') !== -1) {
+      return unwindDeep(array, path);
+    }
+    return unwindShallow(array, path);
   }
-  if (path.indexOf('.') !== -1) {
-    return unwindDeep(array, path);
+
+  function unwindShallow(array, prop) {
+    return array
+      .reduce((acc, curr) => [...acc, ...curr[prop]
+        .map(x => Object
+          .assign({}, curr, { [prop]: x }),
+        )],
+      []);
   }
-  return unwindShallow(array, path);
-}
 
-function unwindShallow(array, prop) {
-  return array
-    .reduce((acc, curr) => [...acc, ...curr[prop]
-      .map(x => Object
-        .assign({}, curr, { [prop]: x }),
-      )], 
-    []);
-}
+  function unwindDeep(array, path) {
+    const shifted = path.split('.');
+    const prop = shifted.shift();
+    const nextLevel = shifted.join('.');
+    const result = array
+      .reduce((acc, curr) => [...acc, ...curr[prop]
+        .map(x => Object
+          .assign({}, curr, { [prop]: unwind(x, nextLevel) }),
+        )],
+      []);
+    return unwind(result, prop);
+  }
 
-function unwindDeep(array, path) {
-  const shifted = path.split('.');
-  const prop = shifted.shift();
-  const nextLevel = shifted.join('.');
-  const result = array
-    .reduce((acc, curr) => [...acc, ...curr[prop]
-      .map(x => Object
-        .assign({}, curr, { [prop]: unwind(x, nextLevel) }),
-      )], 
-    []);
-  return unwind(result, prop);
-}
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = unwind;
+  }
+  return unwind;
+}());
